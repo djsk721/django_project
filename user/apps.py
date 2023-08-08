b3719c7e7b1a1843a1bc56c6740a62a04cfb96e6
DESKTOP-0J10CUH\kimju <djsk721@naver.com>
2023-08-08 15:07:44 +0900

    feat: 기본 구조 적용

--- user/apps.py
+++ user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SampleSwaggerConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sample_swagger'
+    name = 'user'
